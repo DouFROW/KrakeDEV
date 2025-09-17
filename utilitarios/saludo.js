19b7648a4feb95a409a37126546e575b94213003
DouFROW <douglascornejo833@gmail.com>
2025-09-17 12:26:00 -0500

    Funcion recuperarTexto ok

---
+++ utilitarios/saludo.js
@@ -0,0 +1,11 @@
+saludar = function(){
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+}
+
+recuperarTexto = function(idComponente) {
+    let componente = document.getElementById(idComponente);
+    let valorIngresado = componente.value;
+    return valorIngresado;
+}   
+
