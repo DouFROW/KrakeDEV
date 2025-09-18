a0cd21a0435aee664d3c95050445671d44854812
DouFROW <douglascornejo833@gmail.com>
2025-09-17 22:23:30 -0500

    Funcion recuperarInt y recuperarFloat agregadas

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,6 +1,8 @@
 saludar = function(){
     let nombre = recuperarTexto("txtNombre");
     let apellido = recuperarTexto("txtApellido");
+    let entero = recuperarEntero("txtEdad");
+    let float = recuperarFloat("txtEstatura");
 }
 
 recuperarTexto = function(idComponente) {
@@ -8,4 +10,16 @@ recuperarTexto = function(idComponente) {
     let valorIngresado = componente.value;
     return valorIngresado;
 }   
+
+recuperarEntero = function(idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+}
 
