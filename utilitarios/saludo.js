ce1d4c468d9f96f0905070411d442051771f0f4f
DouFROW <douglascornejo833@gmail.com>
2025-09-17 23:20:07 -0500

    Se separaron las funciones a utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -11,37 +11,4 @@ saludar = function(){
     mostrarTextoEnCaja("txtNombre","");
 }
 
-recuperarTexto = function(idComponente) {
-    let componente = document.getElementById(idComponente);
-    let valorIngresado = componente.value;
-    return valorIngresado;
-}   
-
-recuperarEntero = function(idComponente) {
-    let valorCaja = recuperarTexto(idComponente);
-    let valorEntero = parseInt(valorCaja);
-    return valorEntero;
-}
-
-recuperarFloat = function(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorFlotante = parseFloat(valorCaja);
-    return valorFlotante;
-}
-
-mostrarTexto = function(idComponente,mensaje){
-    let valorCaja = document.getElementById(idComponente);
-    valorCaja.innerText = mensaje;
-}
-
-mostrarImagen = function(idComponente,rutaImagen){
-    let valorCaja = document.getElementById(idComponente)
-    valorCaja.src = rutaImagen;
-}
-
-//Limpiar la caja de texto
-mostrarTextoEnCaja = function(idComponente,mensaje){
-    let valorCaja = document.getElementById(idComponente);
-    valorCaja.value = mensaje;
-}
 
