62b62ed2133783042b74736eef46e6f7bd1d9864
DouFROW <douglascornejo833@gmail.com>
2025-10-22 21:40:29 -0500

    Ahorcado - Paso 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,20 +1,40 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-validar = function(){
+let palabraSecreta;
+
+guardarPalabra = function () {
     let caracteres = recuperarTexto("txtSecreta");
-    esMayuscula(caracteres);
+
+    if (caracteres.length < 5 || caracteres.length > 5) {
+        alert("Debe ingresar 5 caracteres.");
+    } else {
+        for (let posicion = 0; posicion < caracteres.length; posicion++) {
+            let vlCaracter = caracteres.charAt(posicion);
+
+            if(!esMayuscula(vlCaracter)){
+                return alert("Todos los caracteres deben ser mayusculas.");
+            }
+        }
+        palabraSecreta = caracteres;
+        console.log(palabraSecreta);
+    }
+
 }
 
 
-esMayuscula = function(caracter){
-    let caracterASCII = caracter.charCodeAt(0);
+esMayuscula = function (caracter) {
+    let caracterASCII = caracter.charCodeAt();
 
-    if(caracterASCII>=65 && caracterASCII<=90){
+    if (caracterASCII >= 65 && caracterASCII <= 90) {
         return true;
-    }else if(caracterASCII==165||caracterASCII==181||caracterASCII==144||caracterASCII==214||caracterASCII==224||caracterASCII==233){
+    } else if (caracterASCII == 209 || caracterASCII == 193 || caracterASCII == 201 || caracterASCII == 205 || caracterASCII == 211 || caracterASCII == 218) {
         return true;
     }
 
-    if(caracterASCII<65 && caracterASCII>90){
+    if (caracterASCII < 65 || caracterASCII > 90) {
         return false;
     }
+
 }
+
+
+
